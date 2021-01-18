6efb6390e41f68883d3625bf4e9a9682bebd4586
aleksey <kupriyanov.al@yandex.ru>
2021-01-18 22:15:20 +0300

    master

--- newsapp/newsapp/settings.py
+++ newsapp/newsapp/settings.py
@@ -55,7 +55,9 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templat
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR,
+                os.path.join(BASE_DIR, 'templates')
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
